6298195e98cb040f55ef74c4cacb4b7893dd3359
PhNgii <johndoe@example.com>
2025-05-27 19:53:07 +0700

    up

---
+++ PlayfairCipherForm.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace Playfair
+{
+    partial class PlayfairCipherForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PlayfairCipherForm));
+            this.labelKey = new System.Windows.Forms.Label();
+            this.txtKeyword = new System.Windows.Forms.TextBox();
+            this.labelPlainText = new System.Windows.Forms.Label();
+            this.txtPlainText = new System.Windows.Forms.TextBox();
+            this.labelCipherText = new System.Windows.Forms.Label();
+            this.txtCipherText = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.cmbAction = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.dgvMatrix = new System.Windows.Forms.DataGridView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMatrix)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelKey
+            // 
+            this.labelKey.AutoSize = true;
+            this.labelKey.Font = new System.Drawing.Font("Segoe UI Emoji", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelKey.Location = new System.Drawing.Point(13, 22);
+            this.labelKey.Name = "labelKey";
+            this.labelKey.Size = new System.Drawing.Size(86, 24);
+            this.labelKey.TabIndex = 0;
+            this.labelKey.Text = "Keyword";
+            // 
+            // txtKeyword
+            // 
+            this.txtKeyword.BackColor = System.Drawing.Color.Bisque;
+            this.txtKeyword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtKeyword.Location = new System.Drawing.Point(118, 22);
+            this.txtKeyword.Multiline = true;
+            this.txtKeyword.Name = "txtKeyword";
+            this.txtKeyword.Size = new System.Drawing.Size(303, 46);
+            this.txtKeyword.TabIndex = 1;
+            // 
+            // labelPlainText
+            // 
+            this.labelPlainText.AutoSize = true;
+            this.labelPlainText.Font = new System.Drawing.Font("Segoe UI Emoji", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPlainText.Location = new System.Drawing.Point(14, 85);
+            this.labelPlainText.Name = "labelPlainText";
+            this.labelPlainText.Size = new System.Drawing.Size(85, 24);
+            this.labelPlainText.TabIndex = 2;
+            this.labelPlainText.Text = "Plaintext";
+            // 
+            // txtPlainText
+            // 
+            this.txtPlainText.BackColor = System.Drawing.Color.Bisque;
+            this.txtPlainText.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPlainText.Location = new System.Drawing.Point(118, 85);
+            this.txtPlainText.Multiline = true;
+            this.txtPlainText.Name = "txtPlainText";
+            this.txtPlainText.Size = new System.Drawing.Size(303, 106);
+            this.txtPlainText.TabIndex = 3;
+            // 
+            // labelCipherText
+            // 
+            this.labelCipherText.AutoSize = true;
+            this.labelCipherText.Font = new System.Drawing.Font("Segoe UI Emoji", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCipherText.Location = new System.Drawing.Point(12, 227);
+            this.labelCipherText.Name = "labelCipherText";
+            this.labelCipherText.Size = new System.Drawing.Size(100, 24);
+            this.labelCipherText.TabIndex = 5;
+            this.labelCipherText.Text = "Ciphertext";
+           
+            // 
+            // txtCipherText
+            // 
+            this.txtCipherText.BackColor = System.Drawing.Color.Bisque;
+            this.txtCipherText.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtCipherText.Location = new System.Drawing.Point(118, 234);
+            this.txtCipherText.Multiline = true;
+            this.txtCipherText.Name = "txtCipherText";
+            this.txtCipherText.ReadOnly = true;
+            this.txtCipherText.Size = new System.Drawing.Size(303, 60);
+            this.txtCipherText.TabIndex = 6;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.Bisque;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(16, 524);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(110, 47);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click_1);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Bisque;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(316, 524);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(104, 47);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // cmbAction
+            // 
+            this.cmbAction.BackColor = System.Drawing.Color.Bisque;
+            this.cmbAction.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmbAction.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbAction.FormattingEnabled = true;
+            this.cmbAction.Items.AddRange(new object[] {
+            "Encrypt",
+            "Decrypt"});
+            this.cmbAction.Location = new System.Drawing.Point(227, 197);
+            this.cmbAction.Name = "cmbAction";
+            this.cmbAction.Size = new System.Drawing.Size(121, 31);
+            this.cmbAction.TabIndex = 9;
+            this.cmbAction.Text = "Encrypt";
+            this.cmbAction.SelectedIndexChanged += new System.EventHandler(this.cmbAction_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Emoji", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 323);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 24);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Playfair square";
+            
+            // 
+            // dgvMatrix
+            // 
+            this.dgvMatrix.BackgroundColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.dgvMatrix.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvMatrix.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMatrix.Location = new System.Drawing.Point(159, 323);
+            this.dgvMatrix.Name = "dgvMatrix";
+            this.dgvMatrix.RowHeadersWidth = 51;
+            this.dgvMatrix.RowTemplate.Height = 24;
+            this.dgvMatrix.Size = new System.Drawing.Size(245, 165);
+            this.dgvMatrix.TabIndex = 11;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.OldLace;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(18, 369);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(135, 119);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Bisque;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(327, 534);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(21, 25);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 13;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Bisque;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(28, 534);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(21, 25);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 14;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(17, 126);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(95, 77);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 15;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(367, 197);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(37, 31);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 16;
+            this.pictureBox5.TabStop = false;
+            // 
+            // PlayfairCipherForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.BackColor = System.Drawing.Color.OldLace;
+            this.ClientSize = new System.Drawing.Size(432, 603);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dgvMatrix);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbAction);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtCipherText);
+            this.Controls.Add(this.labelCipherText);
+            this.Controls.Add(this.txtPlainText);
+            this.Controls.Add(this.labelPlainText);
+            this.Controls.Add(this.txtKeyword);
+            this.Controls.Add(this.labelKey);
+            this.Controls.Add(this.btnClear);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "PlayfairCipherForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Playfair Cipher";
+           
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMatrix)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelKey;
+        private System.Windows.Forms.TextBox txtKeyword;
+        private System.Windows.Forms.Label labelPlainText;
+        private System.Windows.Forms.TextBox txtPlainText;
+        private System.Windows.Forms.Label labelCipherText;
+        private System.Windows.Forms.TextBox txtCipherText;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ComboBox cmbAction;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.DataGridView dgvMatrix;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+    }
+}
+
---
+++ PlayfairCipherForm.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Playfair
+{
+    public partial class PlayfairCipherForm : Form
+    {
+        public PlayfairCipherForm()
+        {
+            InitializeComponent();
+        }
+        private char[,] matrixToDraw;
+        private char[,] GenerateMatrix(string keyword)
+        {
+            string result = "";
+            HashSet<char> used = new HashSet<char>();
+            keyword = keyword.ToUpper().Replace("J", "I");
+
+            foreach (char c in keyword)
+            {
+                if (char.IsLetter(c) && !used.Contains(c))
+                {
+                    result += c;
+                    used.Add(c);
+                }
+            }
+            for (char c = 'A'; c <= 'Z'; c++)
+            {
+                if (c == 'J') continue;
+                if (!used.Contains(c))
+                {
+                    result += c;
+                    used.Add(c);
+                }
+            }
+            char[,] matrix = new char[5, 5];
+            for (int i = 0; i < 25; i++)
+            {
+                matrix[i / 5, i % 5] = result[i];
+            }
+            return matrix;
+        }
+        private List<(char, char)> PrepareTextEncrypt(string plaintext)
+        {
+            plaintext = plaintext.ToUpper().Replace("J", "I").Replace(" ", "");
+            List<(char, char)> pairs = new List<(char, char)>();
+            int i = 0;
+            while (i < plaintext.Length)
+            {
+                char a = plaintext[i];
+                char b = (i + 1) < plaintext.Length ? plaintext[i + 1] : 'X';
+
+                if (a == b)
+                {
+                    pairs.Add((a, 'X'));
+                    i++;
+                }
+                else
+                {
+                    pairs.Add((a, b));
+                    i += 2;
+                }
+            }
+            return pairs;
+        }
+        private List<(char, char)> PrepareTextDecrypt(string ciphertext)
+        {
+            ciphertext = ciphertext.ToUpper().Replace("J", "I").Replace(" ", "");
+            List<(char, char)> pairs = new List<(char, char)>();
+            for (int i = 0; i < ciphertext.Length; i += 2)
+            {
+                char a = ciphertext[i];
+                char b = (i + 1) < ciphertext.Length ? ciphertext[i + 1] : 'X';
+                pairs.Add((a, b));
+            }
+            return pairs;
+        }
+        private (int, int) FindPosition(char[,] matrix, char ch)
+        {
+            for (int row = 0; row < 5; row++)
+            {
+                for (int col = 0; col < 5; col++)
+                {
+                    if (matrix[row, col] == ch)
+                        return (row, col);
+                }
+            }
+            return (-1, -1);
+        }
+        private string EncryptPlayfair(string keyword, string plaintext)
+        {
+            char[,] matrix = GenerateMatrix(keyword);
+            matrixToDraw = matrix;
+            dgvMatrix.Invalidate();
+            DisplayMatrix(matrixToDraw); 
+            var pairs = PrepareTextEncrypt(plaintext);
+            string ciphertext = "";
+
+            foreach (var pair in pairs)
+            {
+                var (row1, col1) = FindPosition(matrix, pair.Item1);
+                var (row2, col2) = FindPosition(matrix, pair.Item2);
+
+                if (row1 == row2)
+                {
+                    ciphertext += matrix[row1, (col1 + 1) % 5];
+                    ciphertext += matrix[row2, (col2 + 1) % 5];
+                }
+                else if (col1 == col2)
+                {
+                    ciphertext += matrix[(row1 + 1) % 5, col1];
+                    ciphertext += matrix[(row2 + 1) % 5, col2];
+                }
+                else
+                {
+                    ciphertext += matrix[row1, col2];
+                    ciphertext += matrix[row2, col1];
+                }
+            }
+            return ciphertext;
+        }
+        private string DecryptPlayfair(string keyword, string ciphertext)
+        {
+            char[,] matrix = GenerateMatrix(keyword);
+            matrixToDraw = matrix;
+            dgvMatrix.Invalidate();
+            DisplayMatrix(matrixToDraw); 
+
+            var pairs = PrepareTextDecrypt(ciphertext);
+            string plaintext = "";
+
+            foreach (var pair in pairs)
+            {
+                var (row1, col1) = FindPosition(matrix, pair.Item1);
+                var (row2, col2) = FindPosition(matrix, pair.Item2);
+
+                if (row1 == row2)
+                {
+                    plaintext += matrix[row1, (col1 + 4) % 5];
+                    plaintext += matrix[row2, (col2 + 4) % 5];
+                }
+                else if (col1 == col2)
+                {
+                    plaintext += matrix[(row1 + 4) % 5, col1];
+                    plaintext += matrix[(row2 + 4) % 5, col2];
+                }
+                else
+                {
+                    plaintext += matrix[row1, col2];
+                    plaintext += matrix[row2, col1];
+                }
+            }
+            return plaintext;
+        }
+        private void DisplayMatrix(char[,] matrix)
+        {
+            dgvMatrix.Rows.Clear();
+            dgvMatrix.Columns.Clear();
+
+            dgvMatrix.AllowUserToResizeColumns = false;
+            dgvMatrix.AllowUserToResizeRows = false;
+            dgvMatrix.ScrollBars = ScrollBars.None;
+            dgvMatrix.BackgroundColor = Color.White;
+            dgvMatrix.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            for (int i = 0; i < 5; i++)
+            {
+                dgvMatrix.Columns.Add($"Col{i}", "");
+                dgvMatrix.Columns[i].Width = 30;
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                dgvMatrix.Rows.Add();
+                dgvMatrix.Rows[i].Height = 30;
+                for (int j = 0; j < 5; j++)
+                {
+                    dgvMatrix.Rows[i].Cells[j].Value = matrix[i, j];
+                }
+            }
+            dgvMatrix.Width = dgvMatrix.Columns.GetColumnsWidth(DataGridViewElementStates.Visible) + dgvMatrix.RowHeadersWidth + 2;
+            dgvMatrix.Height = dgvMatrix.Rows.GetRowsHeight(DataGridViewElementStates.Visible) + dgvMatrix.ColumnHeadersHeight + 2;
+        }
+        private void cmbAction_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtKeyword.Text))
+            {
+                MessageBox.Show("Please enter a keyword.");
+                return;
+            }
+            string keyword = txtKeyword.Text;
+            string inputText = txtPlainText.Text;
+
+            if (cmbAction.SelectedItem.ToString() == "Encrypt")
+            {
+                txtCipherText.Text = EncryptPlayfair(keyword, inputText);
+            }
+            else if (cmbAction.SelectedItem.ToString() == "Decrypt")
+            {
+                txtCipherText.Text = DecryptPlayfair(keyword, inputText);
+            }
+        }
+        private void btnClear_Click_1(object sender, EventArgs e)
+        {
+            txtKeyword.Clear();
+            txtPlainText.Clear();
+            txtCipherText.Clear();
+            matrixToDraw = null;
+            dgvMatrix.Rows.Clear();
+            dgvMatrix.Columns.Clear();
+        }
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+    }
+}
